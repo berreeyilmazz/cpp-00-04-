ab4e8125330601ac45651a15235d434fcc475fb6
Havva Berre Yilmaz <havyilma@k1m43s03.42istanbul.com.tr>
2023-10-20 22:23:04 +0300

    just started

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: havyilma <havyilma@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/20 20:23:11 by havyilma          #+#    #+#             */
+/*   Updated: 2023/10/20 22:20:45 by havyilma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+using namespace std;
+
+// https://youtu.be/-UWIpVIcz_0?si=kz24Cx2rxO3LR4CN
+
+int main()
+{
+//	cout << "Hello world!" << endl;
+//	int a = 422;
+//	cout << "the value of a:" << a << endl;
+//	cout << "toplam: " << toplam << endl;
+//	cout << "toplam: " << float(423 / 20);  a *= 2;
+	int x,y,z;
+	cout << "birinci sayıyı giriniz : ";
+	cin >> x;
+	cout << "ikinci sayıyı giriniz: ";
+	cin >> y;
+	cout << "toplam : " << x + y;
+	return 0;
+}
